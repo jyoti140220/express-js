cf6da2cfa764aad60b53dc723c6e2812bb9680c5
jyoti140220 <jyotibhandari20@navgurukul.org>
2021-06-08 09:36:03 +0530

    commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// Hello world!
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+app.listen(3000);
+
+
+
+
+// with different method
+var express = require('express');
+var app = express();
+
+app.get('/hello', function(req, res){
+   res.send("Hello World!");
+});
+
+app.put('/hello', function(req, res){
+   res.send("You just called the post method at '/hello'!\n");
+});
+
+app.listen(3000,()=>{
+    console.log("Server Running..............")
+});
